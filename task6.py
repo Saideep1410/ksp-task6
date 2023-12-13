bdcc1ef9b1445b6c2eb94ecf5269c5aaeb3c2cb8
Saideep1410 <113224570+Saideep1410@users.noreply.github.com>
2023-12-13 13:11:17 +0530

    Add files via upload

---
+++ task6.py
@@ -0,0 +1,68 @@
+print("Hello Guys")
+num=10
+char="ALEX"
+var =7.12
+print(num)
+print(char)
+print(var)
+if (num<=10):
+    print("small number")
+else:
+    print("big number")
+# operators
+
+a=20
+b=10
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+
+
+x=7
+x+=4
+print(x)
+x-=4
+print(x)
+x*=4
+print(x)
+x/=4
+print(x)
+
+
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+
+print(a&b)
+print(a|b)
+print(a^b)
+
+
+print(x>4 and x<10)
+print(x<3 or x<4)
+
+
+i=0
+while i<6:
+  print(i)
+  i+=1
+
+
+for x in range(6):
+  print(x)
+else:
+  print("completed")
+
+
+
+def my_function():
+  print("HAVE A NICE DAY")
+
+my_function()
